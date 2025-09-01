37f1f54e15dbecff288dc57379e61357d51bc81d
margheritabonan <marghebonan@icloud.com>
2025-09-02 00:35:43 +0200

    Time dimension added with slider
    data.cvs is the reference file I used when running it

--- RShiny/server_ui.R
+++ RShiny/server_ui.R
@@ -6,44 +6,56 @@ library(ggplot2)
 
 server <- function(input, output, session){
   
-  uploaded_data   <- reactive({req(input$datafile)
-    read.csv(input$datafile$datapath)})
+  uploaded_data <- reactive({
+    req(input$datafile)
+    read.csv(input$datafile$datapath)
+  })
   
+  # update time slider after data upload
+  observeEvent(uploaded_data(), {
+    df <- uploaded_data()
+    if ("Time" %in% names(df)) {
+      times <- sort(unique(df$Time))
+      updateSliderInput(session, "timepoint", min = min(times), max = max(times), value = min(times), step = 1)
+    }
+  })
   
-  output$plateplot <- renderPlot({req(input$plotButton) 
+  output$plateplot <- renderPlot({
+    req(input$plotButton)
     df <- isolate(uploaded_data())
-
+    
+    # Filter by selected timepoint
+    if ("Time" %in% names(df) && !is.null(input$timepoint)) {
+      df <- df[df$Time == input$timepoint, ]
+    }
+    
     df$Row <- substr(df$Well, 1, 1)
     df$Col <- as.numeric(sub("^[A-Z]", "", df$Well))
     
-    ggplot(df, aes(x=Col, y=Row)) + 
-      geom_tile(aes(fill=Value > input$threshold),color = "purple") + 
+    ggplot(df, aes(x = Col, y = Row)) + 
+      geom_tile(aes(fill = Value > input$threshold), color = "purple") + 
       scale_y_discrete(limits = rev(LETTERS[1:8])) + 
       scale_x_continuous(breaks = 1:12) + 
-      scale_fill_manual(values = c("FALSE" = "white", "TRUE" = "purple"))+
+      scale_fill_manual(values = c("FALSE" = "white", "TRUE" = "purple")) +
       theme_minimal(base_size = 14) + 
-      labs (title = "Well Plate readout", fill = 6) + 
-      theme(panel.grid = element_blank(),axis.title = element_blank())
-    
-    
+      labs(title = paste("Well Plate readout", if ("Time" %in% names(df)) paste("at time", input$timepoint) else ""), fill = "Above threshold") + 
+      theme(panel.grid = element_blank(), axis.title = element_blank())
   })
-  
-  
+
 }
 
 #Ui------------------------------------------------------------------------
 
-
-library(shiny)
-
 ui <- fluidPage(
-  
   titlePanel("CRICKIT analyze"), 
   sidebarLayout(
-    sidebarPanel(fileInput("datafile", "Upload CSV file", accept = ".csv"), 
-                 sliderInput("threshold", "Value Threshold:", min = 0, max = 1, value = 0.5, step = 0.01), 
-                 actionButton("plotButton", "Show plate data")),
-    
+    sidebarPanel(
+      fileInput("datafile", "Upload CSV file", accept = ".csv"), 
+      sliderInput("threshold", "Value Threshold:", min = 0, max = 1, value = 0.5, step = 0.01),
+
+      sliderInput("timepoint", "Select Timepoint:", min = 0, max = 3, value = 0, step = 1),
+      actionButton("plotButton", "Show plate data")
+    ),
     mainPanel(plotOutput("plateplot"))
   )
 )
