aa695bb15e1290aa7c5f0360bb1f69a59721a060
margheritabonan <marghebonan@icloud.com>
2025-09-27 19:54:07 +0200

    Update time_analysis_server.R

--- RShiny/modules/time_analysis_server.R
+++ RShiny/modules/time_analysis_server.R
@@ -22,45 +22,14 @@ time_analysis_server <- function(id, datasets) {
       wells <- setdiff(names(df), "Time")
       updateSelectInput(session, "vizWell", choices = wells, selected = wells[1])
     })
-    
-    # Example plot using generated data
-    # We need to use input$vizDataset instead
-    # We only use it to name our dataset right now
-    # Instead we should create a plot similar to the one in the excel file
-    # x-axis: Time  y-axis: Well values
 
     output$timePlot <- renderPlot({
       req(input$vizDataset, input$vizWell)
-      
-      # CHANGE THIS
-
-      # Generate example data
-      #set.seed(42)
-      #n_points <- 50
-      
-      # Create sample time-based data
-      #time_points <- seq(1, n_points)
-      #values <- cumsum(rnorm(n_points, 0, 1)) + time_points * 0.1
-
+    
       # get dataset chosen by user
       df <- datasets[[input$vizDataset]]
 
-      
-      # Create data frame
-      #plot_data <- data.frame(
-       # Time = time_points,
-        #Value = values
-      #)
-      
-      #ggplot(plot_data, aes(x = Time, y = Value)) +
-       # geom_line(color = "steelblue", size = 1) +
-       # geom_point(color = "darkblue", size = 2) +
-       # labs(
-        # title = paste("Time Analysis for:", input$vizDataset),
-        #  x = "Time",
-         # y = "Value"
-       # ) +
-       #theme_minimal()
+    
 
       plot_data <- df[, c("Time", input$vizWell), drop = FALSE]
       names(plot_data) <- c("Time", "Value")
